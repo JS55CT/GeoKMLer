c7397181d1dd2a364b43f835c7e842927df9206e
JS55CT <JS55CT.WAZE.CT@GMAIL.COM>
2025-02-16 14:04:58 -0500

    Fource update

--- GeoKMZer.js
+++ GeoKMZer.js
@@ -30,7 +30,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  **************************************************************/
-
 var GeoKMZer = (function () {
   /**
    * GeoKMZer constructor function, which optionally wraps an object.
